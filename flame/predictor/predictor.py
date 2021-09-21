a6c207aa636240ef4d84794445c2979598d87fae
phungpx <phamxuanphungbku@gmail.com>
2021-09-21 11:56:58 +0700

    Update anchor generation

--- flame/predictor/predictor.py
+++ flame/predictor/predictor.py
@@ -2,6 +2,9 @@ import cv2
 import torch
 import numpy as np
 
+from torch import nn, Tensor
+from typing import List, Tuple, Dict, Union, Optional, Generator
+
 
 def chunks(lst: list, size: Optional[int] = None) -> Union[List, Generator]:
     if size is None:
@@ -14,16 +17,16 @@ def chunks(lst: list, size: Optional[int] = None) -> Union[List, Generator]:
 class Predictor:
     def __init__(
         self,
-        model,
-        weight_path,
-        batch_size,
-        image_size,
-        mean,
-        std,
-        anchors,
-        score_threshold,
-        iou_threshold,
-        device
+        model: nn.Module = None,
+        weight_path: str = None,
+        batch_size: Optional[str] = None,
+        image_size: int = 416,
+        mean: Tuple[float, float, float] = (0., 0., 0.),
+        std: Tuple[float, float, float] = (1., 1., 1.),
+        anchors: List[List[Tuple[float, float]]] = None,
+        score_threshold: float = 0.5,
+        iou_threshold: float = 0.5,
+        device: str = 'cpu',
     ):
         super(Predictor, self).__init__()
         self.device
@@ -40,14 +43,14 @@ class Predictor:
         self.model.to(device).eval()
 
 
-    def __call__(self, images: List[np.ndarray]) -> List[Dict[str, torch.Tensor]]:
+    def __call__(self, images: List[np.ndarray]) -> List[Dict[str, Tensor]]:
         samples = self.preprocess(images)
         preds = self.process(samples)
         outputs = self.postprocess(preds)
 
         return outputs
 
-    def preprocess(self, images: List[np.ndarray]) -> Tuple[List[np.ndarray], List[float]]:
+    def preprocess(self, images: List[np.ndarray]) -> List[np.ndarray]:
         '''
         Args:
             images: list of images (image: H x W x C)
@@ -64,7 +67,7 @@ class Predictor:
 
         return samples
 
-    def process(self, samples):
+    def process(self, samples: List[np.ndarray]) -> Tuple[Tensor, Tensor, Tensor]:
         '''
         Args:
             samples: list of processed images (sample: 416 x 416 x 3)
@@ -79,7 +82,7 @@ class Predictor:
                 5: num pf predicted values for each boxes (tp, tx, ty, tw, th),
                 C: num classes,
         '''
-        preds = []
+        s1_preds, s2_preds, s3_preds = [], [], []
 
         for batch in chunks(samples, size=self.batch_size):
             batch = [torch.from_numpy(batch) for batch in batchs]
@@ -88,11 +91,21 @@ class Predictor:
             batch = (batch.float().div(255.) - self.mean) / self.std
 
             with torch.no_grad():
-                preds += self.model(batch)
+                s1, s2, s3 = self.model(batch)
+
+                s1_preds += torch.split(tensor=s1, split_size_or_sections=1, dim=0)
+                s2_preds += torch.split(tensor=s2, split_size_or_sections=1, dim=0)
+                s3_preds += torch.split(tensor=s3, split_size_or_sections=1, dim=0)
+
+        preds = (
+            torch.cat(s1_preds, dim=0),  # N x 3 x S1 x S1 x (5 + C)
+            torch.cat(s2_preds, dim=0),  # N x 3 x S1 x S1 x (5 + C)
+            torch.cat(s3_preds, dim=0),  # N x 3 x S1 x S1 x (5 + C)
+        )
 
         return preds
 
-    def postprocess(self, preds: Tuple[torch.Tensor]):
+    def postprocess(self, preds: Tuple[Tensor, Tensor, Tensor]) -> List[Dict[str, Tensor]]:
         '''
         Args:
             preds: list of three tensor for three scales (S1=13, S2=26, S3=52),
@@ -104,25 +117,26 @@ class Predictor:
                      each values in list is a dict included informations
                      ('boxes', 'labels', 'scores') of image.
         '''
+        num_samples = preds[0].shape[0]
         batch_boxes, batch_labels, batch_scores = [], [], []
 
         for i, pred in enumerate(preds):
             S = pred.shape[2]  # 13, 26, 52
 
             # anchor: 1 x 3 x 1 x 1 x 2
-            anchor = torch.tensor(self.anchors[i], device=device, dtype=torch.float)  # anchor: 3 x 2
+            anchor = torch.tensor(self.anchors[i], device=self.device, dtype=torch.float)  # anchor: 3 x 2
             anchor = anchor.reshape(1, 3, 1, 1, 2)
 
             # cx, cy: N x 3 x S x S
-            cx = torch.arange(S).repeat(batch_size, 3, S, 1).to(device)
+            cx = torch.arange(S).repeat(num_samples, 3, S, 1).to(device)
             cy = cx.permute(0, 1, 3, 2)
 
             # N x 3 x S x S -> reshape: N x (3 * S * S)
             # score = sigmoid(tp)
-            scores = torch.sigmoid(pred[..., 0]).reshape(batch_size, -1)
+            scores = torch.sigmoid(pred[..., 0]).reshape(num_samples, -1)
 
             # N x 3 x S x S -> reshape: N x (3 * S * S)
-            labels = torch.argmax(pred[..., 5:], dim=-1).reshape(batch_size, -1)
+            labels = torch.argmax(pred[..., 5:], dim=-1).reshape(num_samples, -1)
 
             # xy: N x 3 x S x S x 2 (center of bboxes)
             # bx = sigmoid(tx) + cx, by = sigmoid(ty) + cy
@@ -135,8 +149,9 @@ class Predictor:
             bwh = (image_size * anchor) * torch.exp(pred[..., 3:5])
 
             # boxes (x1 y1 x2 y2 type): N x (3 * S * S) x 4
-            boxes = torch.cat([bxy - bwh / 2, bxy + bwh / 2], dim=-1).reshape(batch_size, -1, 4)
-            boxes = torch.clamp(boxes, min=0, max=image_size)
+            x1y1, x2y2 = bxy - bwh / 2, bxy + bwh / 2  # convert xywh to x1y1x2y2
+            boxes = torch.cat([x1y1, x2y2], dim=-1).reshape(num_samples, -1, 4)
+            boxes = torch.clamp(boxes, min=0, max=image_size)  # clip box within [0, image_size]
 
             batch_boxes.append(boxes)
             batch_labels.append(labels)
@@ -162,9 +177,9 @@ class Predictor:
 
                 continue
 
-            boxes = batch_boxes[batch_id, score_indices, :]  # n_boxes x 4
-            labels = batch_labels[batch_id, score_indices]  # n_labels
-            scores = batch_scores[batch_id, score_indices]  # n_scores
+            boxes = batch_boxes[batch_id, score_indices, :]  # [n x 4]
+            labels = batch_labels[batch_id, score_indices]   # [n]
+            scores = batch_scores[batch_id, score_indices]   # [n]
 
             nms_indices = ops.boxes.batched_nms(
                 boxes=boxes, scores=scores, idxs=labels,
