ab0ad102d1df2e6db94cd605b9f25d78dd347280
phungpx <phamxuanphungbku@gmail.com>
2021-10-10 11:46:41 +0700

    Update training flow

--- flame/core/data/pascal_dataset.py
+++ flame/core/data/pascal_dataset.py
@@ -17,31 +17,20 @@ class PascalDataset(Dataset):
         self,
         VOC2007: Dict[str, str] = None,
         VOC2012: Dict[str, str] = None,
+        image_size: int = 416,
         image_extent: str = '.jpg',
         label_extent: str = '.xml',
         classes: Dict[str, int] = None,
         mean: List[float] = [0.485, 0.456, 0.406],
         std: List[float] = [0.229, 0.224, 0.225],
-        imsize: int = 416,
-        S: List[int] = [13, 26, 52],
-        anchors: List[List[List[float]]] = None,
         transforms: Optional[List] = None
     ):
         super(PascalDataset, self).__init__()
-        self.S = S
-        self.imsize = imsize
         self.classes = classes
+        self.image_size = image_size
         self.std = torch.tensor(std, dtype=torch.float).view(3, 1, 1)
         self.mean = torch.tensor(mean, dtype=torch.float).view(3, 1, 1)
         self.pad_to_square = iaa.PadToSquare(position='right-bottom')
-        self.anchors = torch.tensor(
-            data=anchors[0] + anchors[1] + anchors[2],
-            dtype=torch.float32,
-        )  # 9 x 2, 3 anchors for each scale output tensor
-        self.anchors /= imsize
-        self.num_anchors = self.anchors.shape[0]  # 9
-        self.num_anchors_per_scale = self.num_anchors // 3  # 3
-        self.IGNORE_IOU_THRESHOLD = 0.5
         self.transforms = transforms if transforms else []
 
         # VOC2007
@@ -72,35 +61,61 @@ class PascalDataset(Dataset):
     def __len__(self):
         return len(self.data_pairs)
 
-    def _get_label_info(self, label_path: str) -> Tuple[dict, dict]:
+    def get_annotation_info(self, label_path: str) -> Tuple[dict, dict]:
         tree = ET.parse(str(label_path))
         image_info = {'image_name': tree.find('filename').text,
                       'height': int(tree.find('size').find('height').text),
                       'width': int(tree.find('size').find('width').text),
                       'depth': int(tree.find('size').find('depth').text)}
-        label_info = []
+        annotation_info = []
         objects = tree.findall('object')
         for obj in objects:
             bndbox = obj.find('bndbox')
             bbox = np.int32([bndbox.find('xmin').text, bndbox.find('ymin').text,
                              bndbox.find('xmax').text, bndbox.find('ymax').text])
             label_name = obj.find('name').text
-            label_info.append({'label': label_name, 'bbox': bbox})
+            annotation_info.append({'label': label_name, 'bbox': bbox})
 
-        return image_info, label_info
+        return image_info, annotation_info
 
     def __getitem__(self, idx):
         image_path, label_path = self.data_pairs[idx]
-        _, label_info = self._get_label_info(label_path)
 
+        # Get all annotations
+        _, annotation_info = self.get_annotation_info(label_path)
+
+        # Get image, and then ensure image is RGB
         image = cv2.imread(str(image_path))
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+        # Get image information with image_path and (width, height)
         image_info = [str(image_path), image.shape[1::-1]]
 
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        boxes = [label['bbox'] for label in label_info]
-        labels = [self.classes[label['label']] for label in label_info]
+        if not len(annotation_info):  # Has no annotations
+            # Target
+            target = {
+                'image_id': torch.tensor([idx], dtype=torch.int64),
+                'boxes': torch.tensor([[0., 0., 1., 1.]], dtype=torch.float32),
+                'areas': torch.tensor([0.], dtype=torch.float32),
+                'labels': torch.tensor([-1], dtype=torch.int64),
+            }
 
-        # Pad to square to keep object's ratio
+            # Sample
+            image = cv2.resize(image, dsize=(self.image_size, self.image_size))
+            sample = torch.from_numpy(np.ascontiguousarray(image))
+            sample = sample.permute(2, 0, 1).contiguous()
+            sample = (sample.float().div(255.) - self.mean) / self.std
+
+            # Information
+            print(f'Sample: {image_info[0]} has no labels')
+
+            return sample, target, image_info
+
+        # Get all boxes, all labels from annotation file
+        boxes = [label['bbox'] for label in annotation_info]
+        labels = [self.classes[label['label']] for label in annotation_info]
+
+        # Convert to object of imgaug for augmenting image and boxes
         bbs = BoundingBoxesOnImage(
             [
                 BoundingBox(x1=box[0], y1=box[1], x2=box[2], y2=box[3], label=label)
@@ -112,118 +127,28 @@ class PascalDataset(Dataset):
         for transform in random.sample(self.transforms, k=random.randint(0, len(self.transforms))):
             image, bbs = transform(image=image, bounding_boxes=bbs)
 
-        # Rescale image and bounding boxes
+        # Pad to square image and then Resize image and bounding boxes
         image, bbs = self.pad_to_square(image=image, bounding_boxes=bbs)
-        image, bbs = iaa.Resize(size=self.imsize)(image=image, bounding_boxes=bbs)
+        image, bbs = iaa.Resize(size=self.image_size)(image=image, bounding_boxes=bbs)
         bbs = bbs.on(image)
 
-        # get boxes_info
+        # Get all boxes, labels and area of boxes
         boxes = [[bb.x1, bb.y1, bb.x2, bb.y2] for bb in bbs.bounding_boxes]
         labels = [bb.label for bb in bbs.bounding_boxes]
-
-        # Convert to Torch Tensor
-        iscrowd = torch.zeros((len(labels),), dtype=torch.int64)  # suppose all instances are not crowd
-        labels = torch.tensor(labels, dtype=torch.int64)
-        boxes = torch.tensor(boxes, dtype=torch.float32)
-        image_id = torch.tensor([idx], dtype=torch.int64)
-        area = (boxes[:, 3] - boxes[:, 1]) * (boxes[:, 2] - boxes[:, 0])
+        areas = [(box[3] - box[1]) * (box[2] - box[0]) for box in boxes]
 
         # Target
-        boxes_info = {
-            'image_id': image_id,
-            'boxes': boxes,
-            'labels': labels,
-            'area': area,
-            'iscrowd': iscrowd,
+        target = {
+            'image_id': torch.tensor([idx], dtype=torch.int64),
+            'boxes': torch.tensor(boxes, dtype=torch.float32),
+            'labels': torch.tensor(labels, dtype=torch.int64),
+            'area': torch.tensor(areas, dtype=torch.float32),
+            'iscrowd': torch.zeros((len(labels),), dtype=torch.int64),  # suppose all instances are not crowd
         }
 
-        # convert from Bouding Box Object to boxes (x1, y1, x2, y2, label)
-        bboxes = [[bb.x1, bb.y1, bb.x2, bb.y2, bb.label] for bb in bbs.bounding_boxes]
-
-        # convert box type: [x1, y1, x2, y2, class] to box type: [bx/w, by/h, bw/w, bh/h, class]
-        height, width = image.shape[:2]
-        bboxes = self._xyxy2dcxdcydhdw(bboxes=bboxes, image_height=height, image_width=width)
-
-        # initialize targets: shape [probability, x, y, h, w, class_idx]
-        targets = [torch.zeros(size=(self.num_anchors_per_scale, S, S, 6)) for S in self.S]
-
-        for box in bboxes:
-            iou_anchors = self._iou_width_height(torch.tensor(box[2:4]), self.anchors)
-            anchor_indices = iou_anchors.argsort(descending=True, dim=0)
-            center_x, center_y, width, height, class_idx = box
-            has_anchor = [False, False, False]  # set for 3 scales
-
-            for anchor_idx in anchor_indices:
-                scale_idx = anchor_idx // self.num_anchors_per_scale  # scale_idx = 0 (13x13), 1 (26x26), 2 (52x52)
-                anchor_on_scale = anchor_idx % self.num_anchors_per_scale  # anchor_on_scale = 0, 1, 2
-                S = self.S[scale_idx]
-                i, j = int(S * center_y), int(S * center_x)  # which cell? Ex: S=13, center_x=0.5 --> i=int(13 * 0.5)=6
-                anchor_taken = targets[scale_idx][anchor_on_scale, i, j, 0]
-
-                if not anchor_taken and not has_anchor[scale_idx]:
-                    targets[scale_idx][anchor_on_scale, i, j, 0] = 1  # probability
-
-                    cell_w, cell_h = S * width, S * height
-                    cell_x, cell_y = S * center_x - j, S * center_y - i  # both are between [0, 1]
-                    bounding_box = torch.tensor([cell_x, cell_y, cell_w, cell_h])
-                    targets[scale_idx][anchor_on_scale, i, j, 1:5] = bounding_box  # [x, y, w, h]
-
-                    targets[scale_idx][anchor_on_scale, i, j, 5] = int(class_idx)  # class_idx
-                    has_anchor[scale_idx] = True
-
-                elif not anchor_taken and iou_anchors[anchor_idx] > self.IGNORE_IOU_THRESHOLD:
-                    targets[scale_idx][anchor_on_scale, i, j, 0] = -1  # ignore prediction
-
-        # normalize image
+        # Sample
         sample = torch.from_numpy(np.ascontiguousarray(image))
         sample = sample.permute(2, 0, 1).contiguous()
         sample = (sample.float().div(255.) - self.mean) / self.std
 
-        return sample, tuple(targets), image_info, boxes_info
-
-    def _dcxdcydhdw2xyxy(self, bboxes, image_height, image_width):
-        '''
-        Args:
-            bboxes: list([[bx / iw, by / ih, bw / iw, bh / ih, class_idx], ...])
-            image_height: int
-            image_width: int
-        Returns:
-            bboxes: list([[x1, y1, x2, y2, class_idx], ...])
-        '''
-        bboxes = np.asarray(bboxes)
-        bboxes[:, [0, 2]] = bboxes[:, [0, 2]] * image_width
-        bboxes[:, [1, 3]] = bboxes[:, [1, 3]] * image_height
-        bboxes[:, [0, 1]] = bboxes[:, [0, 1]] - bboxes[:, [2, 3]] / 2
-        bboxes[:, [2, 3]] = bboxes[:, [0, 1]] + bboxes[:, [2, 3]]
-
-        return bboxes.tolist()
-
-    def _xyxy2dcxdcydhdw(self, bboxes, image_height, image_width):
-        '''
-        Args:
-            bboxes: list([[x1, y1, x2, y2, class_idx], ...])
-            image_height: int
-            image_width: int
-        Returns:
-            bboxes: list([[bx / iw, by / ih, bw / iw, bh / ih, class_idx], ...])
-        '''
-        bboxes = np.asarray(bboxes)
-        bboxes[:, [2, 3]] = bboxes[:, [2, 3]] - bboxes[:, [0, 1]]
-        bboxes[:, [0, 1]] = bboxes[:, [0, 1]] + bboxes[:, [2, 3]] / 2
-        bboxes[:, [0, 2]] /= image_width
-        bboxes[:, [1, 3]] /= image_height
-
-        return bboxes.tolist()
-
-    def _iou_width_height(self, boxes1, boxes2):
-        '''
-        Args:
-            boxes1: Tensor width and height of the first bounding boxes
-            boxes2: Tensor width and height of the second bounding boxes
-        Returns:
-            ious: Tensor Intersection over Union of the corresponding boxes
-        '''
-        inter_area = torch.min(boxes1[..., 0], boxes2[..., 0]) * torch.min(boxes1[..., 1], boxes2[..., 1])
-        union_area = boxes1[..., 0] * boxes1[..., 1] + boxes2[..., 0] * boxes2[..., 1] - inter_area
-
-        return inter_area / union_area
+        return sample, target, image_info
--- flame/core/loss/yolov3_loss.py
+++ flame/core/loss/yolov3_loss.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 
+from collections import defaultdict
 from typing import List, Tuple, Dict
 
 from . import loss
@@ -13,21 +14,25 @@ class YOLOv3Loss(loss.LossBase):
         lambda_noobj: int = 10,
         lambda_bbox: int = 1,
         lambda_class: int = 1,
-        input_size: int = 416,
-        anchors: List[List[Tuple[float, float]]] = None,
+        image_size: int = 416,
+        scales: List[int] = [13, 26, 52],
+        anchor_sizes: List[List[Tuple[float, float]]] = None,
     ):
         super(YOLOv3Loss, self).__init__()
-        self.anchors = anchors
-        self.input_size = input_size
         self.lambda_obj = lambda_obj
         self.lambda_bbox = lambda_bbox
         self.lambda_class = lambda_class
         self.lambda_noobj = lambda_noobj
 
+        self.scales = scales
+        self.image_size = image_size
+        self.anchor_sizes = torch.tensor(anchor_sizes, dtype=torch.float32)
+        self.ignore_iou_threshold = 0.5
+
     def forward(
         self,
         preds: Tuple[torch.Tensor],
-        targets: Tuple[torch.Tensor]
+        targets: List[Dict[str, torch.Tensor]],
     ) -> Dict[int, float]:
         '''
         Args:
@@ -47,30 +52,114 @@ class YOLOv3Loss(loss.LossBase):
                 52: float,
             }
         '''
+        self.device = preds[0].device
+        self.anchor_sizes = self.anchor_sizes.to(self.device)
+
+        scale_targets = defaultdict(list)
+        for target in targets:
+            for i, scale_target in enumerate(self.convert_target(target=target)):
+                scale_targets[i].append(scale_target)
+
+        targets = [torch.stack(scale_target, dim=0) for scale_target in scale_targets.values()]
+
         losses = {}
-        for pred, target, anchor in zip(preds, targets, self.anchors):
-            scale = pred.shape[2]
-            anchor = torch.tensor(anchor, dtype=torch.float, device=pred.device)
-            anchor /= (self.input_size / scale)  # grid_size = input_size / scale
-            losses[scale] = self.compute_loss(pred, target, anchor)
+        for pred, target, anchor in zip(preds, targets, self.anchor_sizes):
+            losses[pred.shape[2]] = self.compute_loss(pred, target, anchor)
 
         return losses
 
-    def compute_loss(
-        self,
-        pred: torch.Tensor,
-        target: torch.Tensor,
-        anchor: torch.Tensor
-    ) -> float:
+    def xyxy2xywh(self, boxes):
+        '''convert box type from x1, y1, x2, y2 to cx, cy, w, h
+        Args:
+            boxes: [num_boxes, 4], type of box: (x1, y1, x2, y2)
+        Output:
+            boxes: [num_boxes, 4], type of box: (cx, cy, w, h)
+        '''
+        boxes[..., [2, 3]] = torch.clamp(boxes[..., [2, 3]] - boxes[..., [0, 1]], min=1.)
+        boxes[..., [0, 1]] = boxes[..., [0, 1]] + boxes[..., [2, 3]] / 2
+        return boxes
+
+    def xywh2xyxy(self, boxes):
+        '''convert box type from cx, cy, w, h to x1, y1, x2, y2
+        Args:
+            boxes: [num_boxes, 4], type of box: (cx, cy, w, h)
+        Outputs:
+            boxes: [num_boxes, 4], type of box: (x1, y1, x2, y2)
+        '''
+        boxes[..., [0, 1]] = torch.clamp(boxes[..., [0, 1]] - boxes[..., [2, 3]] / 2, min=0.)
+        boxes[..., [2, 3]] = boxes[..., [0, 1]] + boxes[..., [2, 3]]
+        return boxes
+
+    def convert_target(self, target: Dict[str, torch.Tensor]) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
+        '''
+        Args:
+            target: dictionary
+                {
+                    'boxes': Float32 Tensor[N, 4], box type [x1, y1, x2, y2],
+                    'labels': Int32 Tensor[N],
+                    'areas': Float32 Tensor[N],
+                    'image_id': Int32 Tensor[N],
+                    'is_crowed': Int32 Tensor[N],
+                }
+        Outputs: tuple of three tensors
+            (
+                num_anchors_per_scale x S1 x S1 x 6,  6: [prob, x, y, h, w, label]
+                num_anchors_per_scale x S2 x S2 x 6,
+                num_anchors_per_scale x S2 x S2 x 6,
+            )
+        '''
+        # Get all boxes, labels from target
+        boxes, labels = target['boxes'], target['labels']
+        boxes = self.xyxy2xywh(boxes)
+
+        # initialize targets: Tuple[Tensor[num_anchors_per_scale, S, S, 6]]
+        targets = [
+            torch.zeros(size=(self.anchor_sizes[i].shape[0], self.scales[i], self.scales[i], 6), device=self.device)
+            for i in range(len(self.scales))
+        ]
+
+        for scale_id in range(len(self.scales)):
+            anchor = self.anchor_sizes[scale_id]  # num_anchors_per_scale x 2
+            pw, ph = anchor[:, 0], anchor[:, 1]
+            grid_size = self.image_size // self.scales[scale_id]
+
+            for box, label in zip(boxes, labels):
+                bx, by, bw, bh = box[0], box[1], box[2], box[3]
+                inter_area = torch.min(bw, pw) * torch.min(bh, ph)
+                union_area = bw * bh + pw * ph - inter_area
+                ious = inter_area / union_area
+                anchor_indices = ious.argsort(descending=True, dim=0)  # anchor_indices = 0, 1, 2
+
+                for anchor_id in anchor_indices:
+                    j, i = int(bx // grid_size), int(by // grid_size)  # which cell? Ex: S=13, cx=0.5 --> i=int(13 * 0.5)=6
+                    anchor_taken = targets[scale_id][anchor_id, i, j, 0]
+
+                    if not anchor_taken:
+                        cell_w, cell_h = bw / grid_size, bh / grid_size
+                        cell_x, cell_y = bx / grid_size - j, by / grid_size - i  # both are between [0, 1]
+                        cell_box = torch.tensor([cell_x, cell_y, cell_w, cell_h])
+
+                        targets[scale_id][anchor_id, i, j, 0] = 1  # probability
+                        targets[scale_id][anchor_id, i, j, 1:5] = cell_box  # [x, y, w, h]
+                        targets[scale_id][anchor_id, i, j, 5] = label.item()  # class_idx
+
+                    elif not anchor_taken and ious[anchor_id] > self.ignore_iou_threshold:
+                        targets[scale_id][anchor_id, i, j, 0] = -1  # ignore prediction
+
+        return targets
+
+    def compute_loss(self, pred: torch.Tensor, target: torch.Tensor, anchor: torch.Tensor) -> float:
         '''
         Args:
             pred: N x 3 x S x S x (5 + C), 5: tp, tx, ty, tw, th
             target: N x 3 x S x S x 6, (score(-1 or 0 or 1), x, y, w, h)
-            anchor: 3 x 2, relative to input_size
+            anchor: 3 x 2, relative to image_size
         Outputs:
             loss: float
         '''
-        device = pred.device
+        scale = pred.shape[2]
+        grid_size = self.image_size / scale
+        anchor = anchor / grid_size
 
         # check where obj and noobj (ignore if target == -1)
         obj = target[..., 0] == 1  # Iobj_i
@@ -83,10 +172,12 @@ class YOLOv3Loss(loss.LossBase):
         # object loss
         bxy = torch.sigmoid(pred[..., 1:3])
         bwh = torch.exp(pred[..., 3:5]) * anchor[..., 1:3]
-        pred_boxes = torch.cat([bxy, bwh], dim=-1).to(device)
+        pred_boxes = torch.cat([bxy, bwh], dim=-1)
         true_boxes = target[..., 1:5]
-        ious = self.compute_iou(pred_boxes[obj], true_boxes[obj])
-
+        ious = self.compute_iou(
+            boxes1=self.xywh2xyxy(pred_boxes[obj]),
+            boxes2=self.xywh2xyxy(true_boxes[obj])
+        )
         obj_loss = nn.MSELoss()(torch.sigmoid(pred[..., 0:1][obj]), ious * target[..., 0:1][obj])
 
         # coordinate loss
@@ -107,39 +198,29 @@ class YOLOv3Loss(loss.LossBase):
 
         return loss
 
-    def compute_iou(
-        self,
-        boxes1: torch.Tensor,
-        boxes2: torch.Tensor,
-        box_format: str = "midpoint"
-    ) -> torch.Tensor:
-        """This function calculates intersection over union (iou) given pred boxes and target boxes.
-        Args:
-            boxes1 (tensor): Prediction of Bounding Boxes (BATCH_SIZE, 4)
-            boxes2 (tensor): Correct labels of Bounding Boxes (BATCH_SIZE, 4)
-            box_format (str): midpoint/corners, if boxes (x,y,w,h) or (x1,y1,x2,y2)
-        Output:
-            tensor: Intersection over union for all examples (BATCH_SIZE, 1)
-        """
-        if box_format == "midpoint":
-            boxes1_x1y1 = boxes1[..., 0:2] - boxes1[..., 2:4] / 2
-            boxes1_x2y2 = boxes1[..., 0:2] + boxes1[..., 2:4] / 2
-            boxes2_x1y1 = boxes2[..., 0:2] - boxes2[..., 2:4] / 2
-            boxes2_x2y2 = boxes2[..., 0:2] + boxes2[..., 2:4] / 2
+    def compute_iou(self, boxes1: torch.Tensor, boxes2: torch.Tensor) -> torch.Tensor:
+        '''
+        args:
+            boxes1: [N, 4], box_type: [x1, y1, x2, y2]
+            boxes2: [N, 4], box_type: [x1, y1, x2, y2]
+        output:
+            ious: [N, 1]
+        references: https://pytorch.org/docs/stable/notes/broadcasting.html#broadcasting-semantics
+        '''
+        # calculate intersection areas of boxes1 and boxes2
+        inter_w = torch.min(boxes1[..., 2], boxes2[..., 2]) - torch.max(boxes1[..., 0], boxes2[..., 0])
+        inter_h = torch.min(boxes1[..., 3], boxes2[..., 3]) - torch.max(boxes1[..., 1], boxes2[..., 1])
+        inter_w = torch.clamp(inter_w, min=0.)  # N
+        inter_h = torch.clamp(inter_h, min=0.)  # N
+        inter_areas = inter_w * inter_h  # N
 
-        if box_format == "corners":
-            boxes1_x1y1 = boxes1[..., 0:2]
-            boxes1_x2y2 = boxes1[..., 2:4]
-            boxes2_x1y1 = boxes2[..., 0:2]
-            boxes2_x2y2 = boxes2[..., 2:4]
+        # calculate union areas of boxes1 and boxes2
+        area_boxes1 = (boxes1[..., 2] - boxes1[..., 0]) * (boxes1[..., 3] - boxes1[..., 1])  # N
+        area_boxes2 = (boxes2[..., 2] - boxes2[..., 0]) * (boxes2[..., 3] - boxes2[..., 1])  # N
+        union_areas = area_boxes1 + area_boxes2 - inter_w * inter_h  # N
+        union_areas = torch.clamp(union_areas, min=1e-8)
 
-        x1y1 = torch.max(boxes1_x1y1, boxes2_x1y1)
-        x2y2 = torch.min(boxes1_x2y2, boxes2_x2y2)
+        # calculate ious of boxes1 and boxes2
+        ious = inter_areas / union_areas
 
-        inter_areas = torch.prod((x2y2 - x1y1).clamp(min=0), dim=-1, keepdims=True)
-
-        boxes1_area = torch.prod((boxes1_x2y2 - boxes1_x1y1), dim=-1, keepdims=True).abs()
-        boxes2_area = torch.prod((boxes2_x2y2 - boxes2_x1y1), dim=-1, keepdims=True).abs()
-        union_areas = boxes1_area + boxes2_area - inter_areas
-
-        return inter_areas / (union_areas + 1e-6)
+        return ious.unsqueeze(dim=1)
