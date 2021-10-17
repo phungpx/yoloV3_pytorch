aa7a6160a7d0cad5c7d3d1ccb1b1a05ed1c204eb
PhongPX1603 <phongpx.1603@gmail.com>
2021-10-17 16:33:44 +0700

    up files to train do an

---
+++ flame/core/data/electric_components.py
@@ -0,0 +1,138 @@
+import cv2
+import json
+import torch
+import random
+import numpy as np
+import imgaug.augmenters as iaa
+
+from pathlib import Path
+from natsort import natsorted
+from torch.utils.data import Dataset
+from typing import Dict, Tuple, List, Optional
+from imgaug.augmentables.bbs import BoundingBox, BoundingBoxesOnImage
+
+
+class LabelmeDataset(Dataset):
+    def __init__(
+        self,
+        dirnames: List[str] = None,
+        image_size: int = 416,
+        image_patterns: List[str] = ['*.jpg'],
+        label_patterns: List[str] = ['*.json'],
+        classes: Dict[str, int] = None,
+        mean: List[float] = [0.485, 0.456, 0.406],
+        std: List[float] = [0.229, 0.224, 0.225],
+        transforms: Optional[List] = None,
+    ) -> None:
+        super(LabelmeDataset, self).__init__()
+        self.classes = classes
+        self.image_size = image_size
+        self.std = torch.tensor(std, dtype=torch.float).view(3, 1, 1)
+        self.mean = torch.tensor(mean, dtype=torch.float).view(3, 1, 1)
+        self.pad_to_square = iaa.PadToSquare(position='right-bottom')
+        self.transforms = transforms if transforms else []
+
+        image_paths, label_paths = [], []
+        for dirname in dirnames:
+            for image_pattern in image_patterns:
+                image_paths.extend(Path(dirname).glob('**/{}'.format(image_pattern)))
+            for label_pattern in label_patterns:
+                label_paths.extend(Path(dirname).glob('**/{}'.format(label_pattern)))
+
+        image_paths = natsorted(image_paths, key=lambda x: str(x.stem))
+        label_paths = natsorted(label_paths, key=lambda x: str(x.stem))
+
+        self.data_pairs = [[image, label] for image, label in zip(image_paths, label_paths) if image.stem == label.stem]
+
+        print(f'{Path(dirnames[0]).parent.stem}: {len(self.data_pairs)}')
+
+    def __len__(self):
+        return len(self.data_pairs)
+
+    def _get_label_info(self, lable_path: str, classes: dict) -> Dict:
+        with open(file=lable_path, mode='r', encoding='utf-8') as f:
+            json_info = json.load(f)
+
+        label_info = []
+        for shape in json_info['shapes']:
+            label = shape['label']
+            points = shape['points']
+            if label in self.classes and len(points) > 0:
+                x1 = min([point[0] for point in points])
+                y1 = min([point[1] for point in points])
+                x2 = max([point[0] for point in points])
+                y2 = max([point[1] for point in points])
+                bbox = (x1, y1, x2, y2)
+
+                label_info.append({'label': self.classes[label], 'bbox': bbox})
+
+        if not len(label_info):
+            label_info.append({'label': -1, 'bbox': (0, 0, 1, 1)})
+
+        return label_info
+
+    def __getitem__(self, idx: int) -> Tuple[torch.Tensor, Dict, Tuple[str, Tuple[int, int]]]:
+        image_path, label_path = self.data_pairs[idx]
+        label_info = self._get_label_info(lable_path=str(label_path), classes=self.classes)
+        image = cv2.imread(str(image_path))
+        image_info = (str(image_path), image.shape[1::-1])
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+        if len(label_info) == 1 and label_info[0]['label'] == -1:
+            print(f'Sample {image_info[0]} has no labels')
+
+            # Target
+            target = {
+                'image_id': torch.tensor([idx], dtype=torch.int64),
+                'boxes': torch.tensor([[0., 0., 1., 1.]], dtype=torch.float32),
+                'labels': torch.tensor([-1], dtype=torch.int64),
+            }
+
+            # Sample
+            image = cv2.resize(image, dsize=(self.image_size, self.image_size))
+            sample = torch.from_numpy(np.ascontiguousarray(image))
+            sample = sample.permute(2, 0, 1).contiguous()
+            sample = (sample.float().div(255.) - self.mean) / self.std
+
+            return sample, target, image_info
+
+        boxes = [label['bbox'] for label in label_info]
+        labels = [label['label'] for label in label_info]
+
+        # Pad to square to keep object's ratio
+        bbs = BoundingBoxesOnImage([BoundingBox(x1=box[0], y1=box[1], x2=box[2], y2=box[3], label=label)
+                                    for box, label in zip(boxes, labels)], shape=image.shape)
+        for transform in random.sample(self.transforms, k=random.randint(0, len(self.transforms))):
+            image, bbs = transform(image=image, bounding_boxes=bbs)
+
+        # Rescale image and bounding boxes
+        image, bbs = self.pad_to_square(image=image, bounding_boxes=bbs)
+        image, bbs = iaa.Resize(size=self.image_size)(image=image, bounding_boxes=bbs)
+        bbs = bbs.on(image)
+
+        # Convert from Bouding Box Object to boxes, labels list
+        boxes = [[bb.x1, bb.y1, bb.x2, bb.y2] for bb in bbs.bounding_boxes]
+        labels = [bb.label for bb in bbs.bounding_boxes]
+
+        # Convert to Torch Tensor
+        iscrowd = torch.zeros((len(labels),), dtype=torch.int64)  # suppose all instances are not crowd
+        labels = torch.tensor(labels, dtype=torch.int64)
+        boxes = torch.tensor(boxes, dtype=torch.float32)
+        image_id = torch.tensor([idx], dtype=torch.int64)
+        area = (boxes[:, 3] - boxes[:, 1]) * (boxes[:, 2] - boxes[:, 0])
+
+        # Boxes Info
+        target = {
+            'image_id': image_id,
+            'boxes': boxes,
+            'labels': labels,
+            'area': area,
+            'iscrowd': iscrowd,
+        }
+
+        # Sample
+        sample = torch.from_numpy(np.ascontiguousarray(image))
+        sample = sample.permute(2, 0, 1).contiguous()
+        sample = (sample.float().div(255.) - self.mean) / self.std
+
+        return sample, target, image_info
