fe6576c0fa74a8468da2915b5b090f1c8979439b
PhongPX1603 <phongpx.1603@gmail.com>
2021-10-22 20:47:11 +0700

    finished inference

--- inference/real_time_inference.py
+++ inference/real_time_inference.py
@@ -2,7 +2,6 @@ import cv2
 import argparse
 import numpy as np
 from pathlib import Path
-import matplotlib.pyplot as plt
 
 import utils
 
@@ -12,7 +11,7 @@ if __name__ == '__main__':
 	parser.add_argument('--config', default='config.yaml')
 	parser.add_argument('--type-inference', type=str)
 	parser.add_argument('--input-dir')
-	parser.add_argument('--pattern', type=str)
+	parser.add_argument('--video-output', type=str)
 	parser.add_argument('--show', action='store_true')
 	parser.add_argument('--output-dir', default='output/')	
 	args = parser.parse_args()
@@ -24,58 +23,89 @@ if __name__ == '__main__':
 	predictor = utils.create_instance(config)
 
 	if args.type_inference == 'webcam' or args.type_inference == 'video':
-		cap = cv2.VideoCapture(args.input_dir)
+		if args.type_inference == 'webcam':
+			cap = cv2.VideoCapture(int(args.input_dir))
+		elif args.type_inference == 'video':
+			cap = cv2.VideoCapture(args.input_dir)
 
-		cap.set(3, 640)
-		cap.set(4, 480)
+		success, image = cap.read()
+		video_width, video_height = image.shape[:2]
 
-		
+		fourcc = cv2.VideoWriter_fourcc(*"XVID")
+		out = cv2.VideoWriter(args.video_output, fourcc, 10.0, (video_height, video_width))
 
+		cap.set(3, video_width)
+		cap.set(4, video_height)
 		
 
 		while True:
 			success, image = cap.read()
-			prediction = predictor([image])[0]
 
-			if prediction['labels'] is not None:
-			    thickness = max(image.shape) // 500
-			    fontscale = max(image.shape) / 500
-			    boxes = prediction['boxes'].cpu().numpy().astype(np.int32)
-			    labels = prediction['labels'].cpu().numpy()
-			    scores = prediction['scores'].cpu().numpy()
-			    class_names = prediction['names']
-			    for box, score, class_name in zip(boxes, scores, class_names):
-			        color = (
-			            np.random.randint(200, 255),
-			            np.random.randint(50, 200),
-			            np.random.randint(0, 150)
-			        )
+			if success == True:
+				prediction = predictor([image])[0]
 
-			        cv2.rectangle(
-			            img=image,
-			            pt1=tuple(box[:2]),
-			            pt2=tuple(box[2:]),
-			            color=color,
-			            thickness=thickness
-			        )
+				if prediction['labels'] is not None:
+				    boxes = prediction['boxes'].cpu().numpy().astype(np.int32)
+				    labels = prediction['labels'].cpu().numpy()
+				    scores = prediction['scores'].cpu().numpy()
+				    class_names = prediction['names']
 
-			        cv2.putText(
-			            img=image,
-			            text=f'{class_name}: {score: .4f}',
-			            org=tuple(box[:2]),
-			            fontFace=cv2.FONT_HERSHEY_PLAIN,
-			            fontScale=fontscale,
-			            color=color,
-			            thickness=thickness,
-			            lineType=cv2.LINE_AA)
 
-			cv2.imshow("Output", image)	
-			# cv2.imwrite(str(output_dir.joinpath('pxp_do_an.mp4')), image)
+				    font_scale = max(video_height, video_width) / 1200
+				    box_thickness = max(video_height, video_width) // 500
+				    text_thickness = max(video_height, video_width) // 500
+				    # image_scale = max(video_height, video_width) / self.imsize 
+				    for box, score, class_name in zip(boxes, scores, class_names):
+				        color = (
+				            np.random.randint(200, 255),
+				            np.random.randint(50, 200),
+				            np.random.randint(0, 150)
+				        )
 
-			if cv2.waitKey(30) == ord('q'):
+				        cv2.rectangle(
+				            img=image,
+				            pt1=tuple(box[:2]),
+				            pt2=tuple(box[2:]),
+				            color=color,
+				            thickness=box_thickness
+				        )
 
+				        w_text, h_text = cv2.getTextSize(
+				            f'{class_name}: {score: .4f}',
+				            cv2.FONT_HERSHEY_PLAIN,
+				            font_scale,
+				            text_thickness
+				        )[0]
+
+				        cv2.rectangle(
+				            img=image,
+				            pt1=(box[0], box[1] - int(1.5 * h_text)),
+				            pt2=(box[0] + int(1.1 * w_text), box[1]),
+				            color=color,
+				            thickness=-1
+				        )
+
+				        cv2.putText(
+				            img=image,
+				            text=f'{class_name}: {score * 100: .2f}%',
+				            org=(box[0], box[1] - int(0.5 * h_text)),
+				            fontFace=cv2.FONT_HERSHEY_PLAIN,
+				            fontScale=font_scale,
+				            color=(255, 255, 255),
+				            thickness=text_thickness,
+				            lineType=cv2.LINE_AA)
+				out.write(image)
+
+				cv2.imshow("Output", image)	
+				# cv2.imwrite(str(output_dir.joinpath('pxp_do_an.mp4')), image)
+
+				if cv2.waitKey(30) == ord('q'):
+					break
+			else:
 				break
-		cv2.waitKey(1)
+
+		cap.release()
+		out.release()
 		cv2.destroyAllWindows()
 
 	elif args.type_inference == 'image':
